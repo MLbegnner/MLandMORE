6f090ec34f15a5925acb998789c0ac0ff3a80267
KOMOhub <1035844563@qq.com>
2018-04-23 19:49:49 +0800

    更新，成型板

--- MLandMORE/Lecture_in_stanford/Gausian_Model.py
+++ MLandMORE/Lecture_in_stanford/Gausian_Model.py
@@ -3,6 +3,8 @@
 
 '''
     高斯模型
+    二维向量
+    经测试线性程度较好
 '''
 import numpy as np
 import pandas as pd
@@ -28,35 +30,36 @@ class Gausian(object):
 
     def fit(self):
         #length = len(Y);
-        self.fi=np.sum(df)/self.data_len;
+        self.fi=np.sum(self.Y)/self.data_len;
         index_1=np.where(self.Y==1)
-        self.mu_1=np.sum(self.X[index_1],0)/len(index_1)
+        self.mu_1=np.sum(self.X[index_1],0)/len(index_1[0])
         index_0=np.where(self.Y==0)
-        self.mu_0=np.sum(self.X[index_0],0)/len(index_0)
+        self.mu_0=np.sum(self.X[index_0],0)/len(index_0[0])
         for i in range(self.data_len):
-            if i in index_1:
+            if i in index_1[0]:
                 #k=np.where(index_1==i)
-                self.sigma=self.sigma+np.dot((self.X[i,:]-self.mu_1),(self.X[i,:]-self.mu_1).T)
+                self.sigma=self.sigma+np.dot(np.transpose([self.X[i,:]-self.mu_1]),(self.X[i,:]-self.mu_1).reshape(1,2))
                 pass
             else:
                 #k=np.where(index_0==i)
-                self.sigma=self.sigma+np.dot((self.X[i,:]-self.mu_0),(self.X[i,:]-self.mu_0).T)
+                self.sigma=self.sigma+np.dot(np.transpose([self.X[i,:]-self.mu_0]),(self.X[i,:]-self.mu_0).reshape(1,2))
                 pass
             pass
         pass
     def judge(self,X):
-        p_0=np.exp((-1/2)*dot(dot((X-self.mu_0).T,self.sigma.I),(X-self.mu_0)))/(4*np.square(np.pi)*np.sqtr(np.det(self.sigma)))
-        p_1=np.exp((-1/2)*dot(dot((X-self.mu_1).T,self.sigma.I),(X-self.mu_1)))/(4*np.square(np.pi)*np.sqtr(np.det(self.sigma)))
-        if(p_1>p_0):
-            return 1
-        else:
-            return 0
-        pass
+            p_0=np.exp((-1/2)*np.dot(np.dot((X-self.mu_0).T,np.linalg.inv(self.sigma)),(X-self.mu_0)))/(4*np.square(np.pi)*np.sqrt(np.linalg.det(self.sigma)))
+            p_1=np.exp((-1/2)*np.dot(np.dot((X-self.mu_1).T,np.linalg.inv(self.sigma)),(X-self.mu_1)))/(4*np.square(np.pi)*np.sqrt(np.linalg.det(self.sigma)))
+            if(p_1>p_0):
+                return 1
+            else:
+                return 0
+            pass
     def predict(self,X):
-        return judge(self.X)
+        return self.judge(X)
+
         pass
     pass
-
+    
 
 
 def plot_region(X,Y):
@@ -65,26 +68,52 @@ def plot_region(X,Y):
     Z_n=Y.T
     index_0=np.where(Z_n==0)
     index_1=np.where(Z_n==1)
-    plt.scatter(X_n[index_0],Y_n[index_0],marker='*',color='red')
-    plt.scatter(X_n[index_1],Y_n[index_1],marker='x',color='blue')
+    flag_for_label0=0
+    flag_for_label1=0
+    if flag_for_label0==0:
+        plt.scatter(X_n[index_0],Y_n[index_0],marker='*',color='red',label='origin data : output is 0')
+        flag_for_label0=1
+        pass
+    else:
+        plt.scatter(X_n[index_0],Y_n[index_0],marker='*',color='red')
+        pass
+    if flag_for_label1==0:
+        plt.scatter(X_n[index_1],Y_n[index_1],marker='x',color='blue',label='origin data: output is 1')
+        flag_for_label1=1
+        pass
+    else:
+        plt.scatter(X_n[index_1],Y_n[index_1],marker='x',color='blue')
+        pass
     plt.title("origin data")
     plt.xlabel("X")
     plt.ylabel("Y")
-    plt.show()
     pass
 gas=Gausian()
 gas.getData()
 gas.fit()
-
 plot_region(gas.X,gas.Y)
-result=gas.predict(np.array([5.6,2.4]))
-
-if result==0:
-    plt.scatter(5.6,2.4,marker='x',color='yelow')
-    pass
-else:
-    plt.scatter(5.6,2.4,marker='*',color='yellow')
-    pass
-
+Y_rand=7*np.random.rand(100)
+X_rand=4*np.random.rand(100)+4
+flag_for_label0=0
+flag_for_label1=0
+for i in range(100):
+    result=gas.predict([X_rand[i],Y_rand[i]])
+    if result==0:
+        if flag_for_label0==0:
+            plt.scatter(X_rand[i],Y_rand[i],marker='x',color='yellow',label='predict data : output is 0')
+            flag_for_label0=1;
+            pass
+        else:
+            plt.scatter(X_rand[i],Y_rand[i],marker='x',color='yellow')
+        pass
+    else: 
+        if flag_for_label1==0:
+            plt.scatter(X_rand[i],Y_rand[i],marker='*',color='black',label='predict data : output is 1')
+            flag_for_label1=1
+            pass
+        else:
+            plt.scatter(X_rand[i],Y_rand[i],marker='*',color='black')
+        pass
+plt.legend(loc='upper left',frameon=False)
 plt.show()
 
